1d66bd56ff39bd903f860d7664289401fcc180e1
Natithorn Srimee <natithorn.s@kkumail.com>
2022-11-28 15:33:00 +0700

    Update code by natithorn

--- Lab OOP00A/Lab OOP00A/Program.cs
+++ Lab OOP00A/Lab OOP00A/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Natithorn Srimee");
+Console.WriteLine("653450292-4");
+Console.WriteLine("natithorn.s@kkumail.com");
